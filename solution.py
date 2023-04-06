01346f9c43bc5d0106bcdeff1903c052a7bdb6fd
teensp1rit <55020493+teensp1rit@users.noreply.github.com>
2023-04-06 20:48:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 332487463 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    y = np.array([(i + 27 - np.random.exponential(1)) / 10 for i in x])
+    return y.mean() # Ваш ответ
